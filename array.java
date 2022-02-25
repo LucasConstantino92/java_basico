3f7812a8849182f0dd58276b47302387355e3eb2
Lucas Constantino <95923996+LucasConstantino92@users.noreply.github.com>
2022-02-25 09:54:24 -0300

    Create array.java

---
+++ array.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        String[] nomes = {"Lucas", "Michely", "Marcelo", "Ryan", "Luciana", "Vanilda"};
+        int[] numeros = {30, 27, 29, 19, 51};
+
+        System.out.println(nomes[0]);
+    }
+}
